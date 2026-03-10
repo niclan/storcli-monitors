b9db8e66fec24e229446eaa313e0aa577f14a548
Nicolai Langfeldt <nicolai@langfeldt.net>
2026-03-10 22:17:56 +0100

    Refactor to use JSON parsing with storcli64 JSON output, support multiple controllers

--- storcli_temp.pl
+++ storcli_temp.pl
@@ -2,6 +2,7 @@
 
 use strict;
 use warnings;
+use JSON;
 
 # Munin plugin for monitoring MegaRAID controller temperatures
 
@@ -30,29 +31,89 @@ sub config {
     print "graph_vlabel Temperature (°C)\n";
     print "graph_category raid\n";
     print "graph_info This graph shows the temperature of MegaRAID controllers\n";
-    print "temp.label Controller Temperature\n";
+    
+    my @controllers = get_controllers();
+    foreach my $ctrl (@controllers) {
+        my $ctl_id = $ctrl->{ctl};
+        my $model = $ctrl->{model};
+        my $adapter = $ctrl->{adapter};
+        print "temp_ctl$ctl_id.label Controller $ctl_id: $model ($adapter)\n";
+        print "temp_ctl$ctl_id.type GAUGE\n";
+        print "temp_ctl$ctl_id.warning 70\n";
+        print "temp_ctl$ctl_id.critical 80\n";
+    }
+}
+
+sub get_controllers {
+    $STORCLI = find_storcli() unless $STORCLI;
+    return unless $STORCLI;
+    
+    my $output = `$STORCLI show J 2>/dev/null`;
+    return unless $output;
+    
+    my $data = eval { decode_json($output) };
+    return unless $data;
+    
+    my @controllers;
+    if (ref $data->{Controllers} eq 'ARRAY') {
+        foreach my $ctrl (@{$data->{Controllers}}) {
+            my $response = $ctrl->{Response Data} or next;
+            my $overview = $response->{IT System Overview} or next;
+            
+            foreach my $item (@{$overview}) {
+                push @controllers, {
+                    ctl => $item->{Ctl},
+                    model => $item->{Model},
+                    adapter => $item->{AdapterType}
+                };
+            }
+        }
+    }
+    
+    return @controllers;
+}
+
+sub get_temperatures {
+    $STORCLI = find_storcli() unless $STORCLI;
+    return unless $STORCLI;
+    
+    my %temps;
+    
+    my $output = `$STORCLI /call show temperature J 2>/dev/null`;
+    return unless $output;
+    
+    my $data = eval { decode_json($output) };
+    return unless $data;
+    
+    if (ref $data->{Controllers} eq 'ARRAY') {
+        foreach my $ctrl (@{$data->{Controllers}}) {
+            my $response = $ctrl->{Response Data} or next;
+            my $props = $response->{Controller Properties} or next;
+            
+            my $ctl_id = $ctrl->{Command Status}->{Controller};
+            
+            foreach my $prop (@{$props}) {
+                if ($prop->{Ctrl_Prop} =~ /temperature/i) {
+                    $temps{$ctl_id} = $prop->{Value};
+                }
+            }
+        }
+    }
+    
+    return %temps;
 }
 
 sub fetch {
-    $STORCLI = find_storcli();
-    return unless $STORCLI;
+    my %temps = get_temperatures();
     
-    my $output = `$STORCLI /call/eall/show all`;  # command to retrieve data from MegaRAID
-    my @lines = split /\n/, $output;
-    my $temp;
-
-    foreach my $line (@lines) {
-        if ($line =~ /Temperature (\d+)C/) {
-            $temp = $1;
-            last;
-        }
+    foreach my $ctl_id (sort keys %temps) {
+        print "temp_ctl$ctl_id.value $temps{$ctl_id}\n";
     }
-
-    print "temp.value $temp\n" if defined $temp;
 }
 
 sub dirtyconfig {
-    print "config\n";
+    config();
+    fetch();
 }
 
 my $mode = shift || die "Usage: $0 <autoconf|config|fetch|dirtyconfig>\n";
