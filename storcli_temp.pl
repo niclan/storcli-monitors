77708a859821143ea98e327b3e5e44ed727ea005
Nicolai Langfeldt <nicolai@langfeldt.net>
2026-03-10 22:00:56 +0100

    Updating storcli_temp.pl with a complete Munin plugin implementation for MegaRAID controller temperature monitoring.

--- storcli_temp.pl
+++ storcli_temp.pl
@@ -3,42 +3,52 @@
 use strict;
 use warnings;
 
-# Munin Plugin for monitoring MegaRAID controller temperatures
+use Sys::Sysinfo;
+use POSIX;  # For floor function
 
-sub get_temp {
-    my @temps;
-    my $output = `storcli /cx/gtemp`;  # Example command, replace with the correct command
-    foreach my $line (split /\n/, $output) {
-        if ($line =~ /Temperature\s+:(\s*\d+)/) {
-            push @temps, $1;
+# Munin plugin for monitoring MegaRAID controller temperatures
+
+sub autoconf {
+    print "yes\n";
+}
+
+sub config {
+    print "graph_title MegaRAID Controller Temperatures\n";
+    print "graph_vlabel Temperature (°C)\n";
+    print "graph_category raid\n";
+    print "graph_info This graph shows the temperature of MegaRAID controllers\n";
+    print "temp.label Controller Temperature\n";
+}
+
+sub fetch {
+    my $output = `storcli /call/eall/show all`;  # command to retrieve data from MegaRAID
+    my @lines = split /\n/, $output;
+    my $temp;
+
+    foreach my $line (@lines) {
+        if ($line =~ /Temperature (\d+)C/) {
+            $temp = $1;
+            last;
         }
     }
-    return @temps;
+
+    print "temp.value $temp\n";
 }
 
-sub munin_config {
-    print "graph_title MegaRAID Controller Temperature\n";
-    print "graph_vlabel Temperature (C)\n";
-    print "graph_category hardware\n";
-    print "graph_info This plugin monitors the temperatures of MegaRAID controllers.\n";
-
-    for my $i (0..$#{$temps}) {
-        print "temp$i.label Controller $i\n";
-    }
+sub dirtyconfig {
+    print "config\n";
 }
 
-sub munin_value {
-    my @temps = get_temp();
-    for my $i (0..$#temps) {
-        print "temp$i.value $temps[$i]\n";
-    }
-}
+my $mode = shift || die "Usage: $0 <autoconf|config|fetch|dirtyconfig>\n";
 
-my $mode = shift;
-if ($mode eq 'config') {
-    munin_config();
-} elsif ($mode eq 'value') {
-    munin_value();
+if ($mode eq 'autoconf') {
+    autoconf();
+} elsif ($mode eq 'config') {
+    config();
+} elsif ($mode eq 'fetch') {
+    fetch();
+} elsif ($mode eq 'dirtyconfig') {
+    dirtyconfig();
 } else {
     die "Unknown mode: $mode\n";
 }
